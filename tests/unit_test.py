e9e2af34d2891a3e8ae494bcd729094ddbb18b6a
Brent R. Yates <brentRyates@gmail.com>
2025-09-26 09:57:46 -0400

    Project file and updated tests

--- tests/unit_test.py
+++ tests/unit_test.py
@@ -1,9 +1,19 @@
+'''Unit tests for the Book class in library.py'''
+
+import pytest
 from library import Book
 
-b = Book('hi', 'hello there\nhow are you?')
-def test_book():
+@pytest.fixture(scope='module', autouse=True)
+def b():
+    '''Fixture that returns a Book instance for testing'''
+    return Book('hi', 'hello there\nhow are you?')
+
+
+def test_book(b):
+    '''Test the Book class'''
     assert b.title == b.title
 
 
-def test_token():
+def test_token(b):
+    '''Test the tokenization of the Book class'''
     assert b.token == ['hello', 'there', 'how', 'are', 'you?']
